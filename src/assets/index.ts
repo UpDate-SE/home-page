a114eb16ffb4e996f143fd548abada95470d79e4
luis <47650944+Luisill0@users.noreply.github.com>
2023-05-19 13:38:24 -0600

    assets

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as Meeting } from './meeting.jpg';
+export { default as TeamProcess } from './team-process.jpg';
+export { default as TrenningLogo } from './trenning.jpg';
+export { default as UpdateSELogo } from './updateSELogo.png';
