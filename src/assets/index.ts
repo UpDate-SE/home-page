b8795e7b9ec99ff91a7a760fa1a6b8d30ec3f9a7
luis <47650944+Luisill0@users.noreply.github.com>
2023-05-22 13:07:40 -0600

    logos

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
+export { default as ACSELogo } from './ACSELogo.png';
+export { default as InmomatchLogo } from './inmomatch.jpg';
 export { default as Meeting } from './meeting.jpg';
+export { default as RentFirmeLogo } from './rentefirme.jpg';
 export { default as TeamProcess } from './team-process.jpg';
 export { default as TrenningLogo } from './trenning.jpg';
-export { default as UpdateSELogo } from './updateSELogo.png';
