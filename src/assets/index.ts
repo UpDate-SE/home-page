925ad2823539cf9e146bc30f340548f146723996
luis <47650944+Luisill0@users.noreply.github.com>
2023-06-21 12:41:11 -0600

    assets for plans

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,3 +7,4 @@ export { default as TeamProcess } from './team-process.webp';
 export { default as TeamProcessSM } from './team-process-sm.webp';
 export { default as TrenningLogo } from './trenning.webp';
 export { default as ZoomerChatLogo } from './zoomer.webp';
+export * from './plans';
