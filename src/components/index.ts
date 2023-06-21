b5c7d15ed5d2c4150cf70ac58de07ee3fc7f6886
luis <47650944+Luisill0@users.noreply.github.com>
2023-06-21 12:40:08 -0600

    index for new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 export { default as CarouselHome } from './CarouselHome';
 export { default as DarkModeToggler } from './DarkModeToggler';
 export { default as FooterHome } from './Footer';
+export { default as MarketingPlans } from './MarketingPlans';
 export { default as NavBarHome } from './NavbarHome';
+export { default as PackageInfo } from './PackageInfo';
+export { default as PackagesCarousel } from './PackagesCarousel';
