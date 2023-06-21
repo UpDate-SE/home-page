931e0071391f56557eeed2173168e9c3aef80d32
luis <47650944+Luisill0@users.noreply.github.com>
2023-06-21 12:41:24 -0600

    add plans to homepage

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,15 +1,16 @@
 import { useContext, useEffect, useState } from 'react';
 import { Button, Col, Container, Row } from 'reactstrap';
 
-import { CaorIncLogo, InmomatchLogo, Meeting, RentFirmeLogo, TeamProcess, TeamProcessSM, TrenningLogo, ZoomerChatLogo } from 'assets';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-import { CarouselHome, NavBarHome } from 'components';
+import { CaorIncLogo, InmomatchLogo, KeepCommunity, MakeKnown, Marketing1, Marketing2, Marketing3, MarketingExpert, Meeting, Networks1, Networks2, Networks3, RentFirmeLogo, StarterGraph, TeamProcess, TeamProcessSM, TrenningLogo, ZoomerChatLogo } from 'assets';
+import { CarouselHome, FooterHome, MarketingPlans, NavBarHome, PackageInfo, PackagesCarousel } from 'components';
 import { UserContext } from 'context';
 import { UserContextType, WindowDimensions } from '@types';
 
 import 'scss/css/style.css';
 import 'styles/HomePage.css';
-import Footer from 'components/Footer';
 
 const HomePage = () => {
     const { darkMode, currentLang } = useContext(UserContext) as UserContextType; 
@@ -50,9 +51,18 @@ const HomePage = () => {
                             'Do you have a project you want to make and you don\'t know how?'
                         }
                     </span>
-                    <a href='mailto:josuearredondo@advancedcodese.com'>
+                    <a href='mailto:josuearredondo@advancedcodese.com'
+                        className='text-decoration-none'
+                    >
                         <Button color={darkMode ? 'primary-dark' : 'primary'} 
-                            className='fw-bold text-uppercase p-3 mt-5'>
+                            className='fw-bold text-uppercase p-3 mt-5
+                                d-flex align-items-center
+                            '
+                        >
+                                <FontAwesomeIcon
+                                    icon={faEnvelope}
+                                    className='me-2 fs-3'
+                                />
                                 {currentLang.language === 'ESP' ?
                                     'Cotiza tu Proyecto'
                                     :
@@ -61,8 +71,264 @@ const HomePage = () => {
                         </Button>
                     </a>
                 </Container>
-                <Container fluid 
-                    id='us-section'
+                <Container fluid id='development'
+                    className='position-relative px-sm-5 mt-2 pt-4'
+                >
+                    <span
+                        className={`${darkMode ? 'text-primary-dark' : 'text-primary'} 
+                            fs-2 fw-bold mb-2 section-title`}
+                    >
+                        {currentLang.language === 'ESP' ?
+                            'Desarrollo todo en uno'
+                            :
+                            'All-in-one development'
+                        }
+                    </span>
+                    <PackageInfo
+                        title={currentLang.language === 'ESP' ?
+                            'Paquete iniciador digital'
+                            :
+                            'Digital startup'
+                        }
+                        perks={
+                            currentLang.language === 'ESP' ?
+                            [
+                                'Página web sencilla.',
+                                'De uno a tres correos empresariales.',
+                                'Hosting de la página web.',
+                                'Dominio personalizado.',
+                                'Paquete "Presencia" de Marketing.',
+                                'Paquete "Presencia" de Redes.',
+                                'Tarjeta inteligente personal.'
+                            ]
+                            :
+                            [
+                                'Simple web page.',
+                                '1-3 company emails.',
+                                'Web page hosting.',
+                                'Customized domain.',
+                                'Includes "Presence" Marketing package.',
+                                'Includes "Presence" Networks package.',
+                                'Personal smart card.'
+                            ]
+                        }
+                        image={StarterGraph}
+                        price={'$10,000 MXN'}
+                    />
+                    <div className='anchor' id='plans'></div>
+                </Container>
+                <Container fluid id='content'
+                    className='px-sm-5 mt-2 pt-3'
+                >
+                    <span
+                        className={`${darkMode ? 'text-primary-dark' : 'text-primary'} 
+                            fs-2 fw-bold mb-2 section-title`}
+                    >
+                        {currentLang.language === 'ESP' ?
+                            'Paquetes de contenido'
+                            :
+                            'Content packages'
+                        }
+                    </span>
+                    <PackagesCarousel
+                        items={[
+                            <PackageInfo
+                                title={currentLang.language === 'ESP' ?
+                                    'Paquete "Date a conocer"'
+                                    :
+                                    '"Make yourself known" Package'
+                                }
+                                perks={
+                                    currentLang.language === 'ESP' ?
+                                    [
+                                        'Plan 1 de Marketing: SEO en pagina web.',
+                                        'Plan 1 de Redes sociales: 3 publicaciones en Instagram y Facebook a la semana y análisis de resultados (12 publicaciones al mes).',
+                                        'En el primer mes se hace un estudio de la marca con un costo de $500 MXN.'
+                                    ]
+                                    :
+                                    [
+                                        'Marketing plan 1: Web page SEO.',
+                                        'Social Networks plan 1: 3 Instagram and Facebook posts a week and results analysis (12 posts a month).',
+                                        'During the first month, a brand study is made with an extra cost of $500 MXN'
+                                    ]
+                                }
+                                image={MakeKnown}
+                                price='$4,500 MXN/mes'
+                            />,
+                            <PackageInfo
+                                title={currentLang.language === 'ESP' ?
+                                    'Paquete "Mantener Comunidad"'
+                                    :
+                                    '"Keeping the Community" Package'
+                                }
+                                perks={
+                                    currentLang.language === 'ESP' ?
+                                    [
+                                        'Plan 2 de Marketing: SEO en pagina web y palabras clave (analytics).',
+                                        'Plan 2 de Redes sociales: 5 publicaciones en Instagram y Facebook a la semana y análisis de resultados (20 publicaciones al mes).',
+                                        'En el primer mes se hace un estudio de la marca con un costo de $500 MXN.'
+                                    ]
+                                    :
+                                    [
+                                        'Marketing plan 2: Web page SEO and keywords (analytics).',
+                                        'Social Networks plan 1: 5 Instagram and Facebook posts a week and results analysis (20 posts a month).',
+                                        'During the first month, a brand study is made with an extra cost of $500 MXN'
+                                    ]
+                                }
+                                image={KeepCommunity}
+                                price='$8,000* MXN/mes'
+                            />,
+                            <PackageInfo
+                                title={currentLang.language === 'ESP' ?
+                                    'Paquete "Experto del Marketing"'
+                                    :
+                                    '"Marketing Expert" Package'
+                                }
+                                perks={
+                                    currentLang.language === 'ESP' ?
+                                    [
+                                        'Plan 3 de Marketing: SEO en pagina web, palabras clave y SEM (500 destinados para campañas de anuncios).',
+                                        'Plan 3 de Redes sociales: 10 publicaciones en Instagram y Facebook a la semana y análisis de resultados (40 publicaciones al mes).',
+                                        'En el primer mes se hace un estudio de la marca con un costo de $500 MXN.'
+                                    ]
+                                    :
+                                    [
+                                        'Marketing plan 2: Web page SEO, keywords and SEM (500 allocated towards ad campaings).',
+                                        'Social Networks plan 2: 10 Instagram and Facebook posts a week and results analysis (40 posts a month).',
+                                        'During the first month, a brand study is made with an extra cost of $500 MXN'
+                                    ]
+                                }
+                                image={MarketingExpert}
+                                price='$13,000* MXN/mes'
+                            />
+                        ]}
+                    />
+                </Container>
+                <Container fluid id='marketing'
+                    className='px-sm-5 mt-3 pt-2'
+                >
+                    <span 
+                        className={`${darkMode ? 'text-primary-dark' : 'text-primary'}
+                            fs-2 fw-bold my-2 section-title`
+                        }
+                    >
+                        {currentLang.language === 'ESP' ?
+                            'Marketing en páginas web'
+                            :
+                            'Web page marketing'
+                        }
+                    </span>
+                    <MarketingPlans
+                        name='Marketing'
+                        plans={[
+                            {
+                                image: Marketing1,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    'SEO en página web.'
+                                ]
+                                :
+                                [
+                                    'SEO in web page.'
+                                ],
+                                price: '$2,500* MXN/mes'
+                            },
+                            {
+                                image: Marketing2,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    'SEO en página web.',
+                                    'Palabras clave.'
+                                ]
+                                :
+                                [
+                                    'SEO in web page.',
+                                    'Keywords.'
+                                ],
+                                price: '$3,500* MXN/mes'
+                            },
+                            {
+                                image: Marketing3,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    'SEO en página web.',
+                                    'Palabras clave.',
+                                    'SEM (500 para anuncios).'
+                                ]
+                                :
+                                [
+                                    'SEO in web page',
+                                    'Keywords.',
+                                    'SEM (500 for ads).'
+                                ],
+                                price: '$4,500* MXN/mes'
+                            }
+                        ]}
+                    />
+                </Container>
+                <Container fluid id='social-networks'
+                    className='px-sm-5 mt-3 pt-2'
+                >
+                    <span 
+                        className={`${darkMode ? 'text-primary-dark' : 'text-primary'}
+                            fs-2 fw-bold my-2 section-title`
+                        }
+                    >
+                        {currentLang.language === 'ESP' ?
+                            'Marketing en redes sociales'
+                            :
+                            'Social networks marketing'
+                        }
+                    </span>
+                    <MarketingPlans
+                        name={currentLang.language === 'ESP' ? 'Redes' : 'Networks'}
+                        plans={[
+                            {
+                                image: Networks1,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    '3 publicaciones en Facebook e Instagram a la semana.',
+                                    'Análisis de resultados.'
+                                ]
+                                :
+                                [
+                                    '3 Facebook and Instagram posts a week.',
+                                    'Analysis of results.'
+                                ],
+                                price: '$2,500* MXN/mes'
+                            },
+                            {
+                                image: Networks2,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    '5 publicaciones en Facebook e Instagram a la semana.',
+                                    'Análisis de resultados.'
+                                ]
+                                :
+                                [
+                                    '5 Facebook and Instagram posts a week.',
+                                    'Analysis of results.'
+                                ],
+                                price: '$5,000* MXN/mes'
+                            },
+                            {
+                                image: Networks3,
+                                perks: currentLang.language === 'ESP' ? 
+                                [
+                                    '10 publicaciones en Facebook e Instagram a la semana.',
+                                    'Análisis de resultados.'
+                                ]
+                                :
+                                [
+                                    '10 Facebook and Instagram posts a week.',
+                                    'Analysis of results.'
+                                ],
+                                price: '$9,000* MXN/mes'
+                            }
+                        ]}
+                    />
+                </Container>
+                <Container fluid id='us-section'
                     className='position-relative px-sm-5 mt-2 pt-4'
                 >
                     <Row>
@@ -275,7 +541,7 @@ const HomePage = () => {
                     <div className='anchor' id='contact'></div>
                 </Container>
             </Container>
-            <Footer/>
+            <FooterHome/>
         </Container>
     )
 }
