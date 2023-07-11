8d6eb72888caf5bd36532709c50586fb70c38e78
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-11 01:13:22 -0600

    interfaces

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -18,10 +18,16 @@ export interface UserContextType {
     langOption: UserLanguage;
     toggleDarkMode: () => void;
     toggleLanguage: () => void;
+    login: (credentials: LoginCredentials) => Promise<boolean>;
 }
 
 export type MarketingPlanType = {
     image: string;
     perks: Array<string>;
     price: string;
+}
+
+export type LoginCredentials = {
+    username: string;
+    password: string;
 }
