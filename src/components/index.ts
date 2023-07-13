21d51279bc29aed3b480a22fc73c97d25918f5ab
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-13 16:36:12 -0600

    components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,8 @@ export { default as CarouselHome } from './CarouselHome';
 export { default as DarkModeToggler } from './DarkModeToggler';
 export { default as FooterHome } from './Footer';
 export { default as LangSwitcher } from './LangSwitcher';
+export { default as LoginForm } from './LoginForm';
 export { default as NavbarDefault } from './NavbarDefault';
 export { default as NavBarHome } from './NavbarHome';
+export * from './cardForm';
 export * from './plans';
