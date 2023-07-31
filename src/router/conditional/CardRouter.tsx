c0528c1e249a5e817f6c541e279d49c8582ca650
luis <47650944+Luisill0@users.noreply.github.com>
2023-07-31 12:31:43 -0600

    fetch card data in router

--- src/router/conditional/CardRouter.tsx
+++ src/router/conditional/CardRouter.tsx
@@ -1,13 +1,46 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-import { CardPage } from "pages";
+import { CardPage, LoadingPage } from "pages";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "context";
+import { BusinesCardInDB, UserContextType } from "@types";
+import { removeDashes } from "@helpers/card-formatter";
 
 const CardRouter = (): JSX.Element => {
     const { company, name } = useParams();
-    
-    if((!company) || (!name)) return <Navigate to='/' />
+    const { state } = useLocation();
+    const navigate = useNavigate();
 
-    return <CardPage companyName={company} name={name} />;
+    const { getBusinessCard } = useContext(UserContext) as UserContextType;
+
+    const [businessCard, setBusinessCard] = useState<BusinesCardInDB | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if((!company) || (!name)) return navigate('/');
+        
+        if(state?.card) {
+            setBusinessCard(state.card);
+            setLoading(false);
+        }
+        
+        if(!businessCard && !state?.card) {
+            fetchBusinessCardData(company, name);
+        }
+    }, [])
+
+    const fetchBusinessCardData = async (company: string, name:string) => {
+        const businessCardOrNull = await getBusinessCard(removeDashes(company), removeDashes(name));
+        if(!businessCardOrNull) navigate('/');
+        setBusinessCard(businessCardOrNull);
+        setLoading(false);
+    }
+    
+    if(loading || !businessCard) {
+        return <LoadingPage />;
+    }
+
+    return <CardPage businessCard={businessCard} />
 }
 
 export default CardRouter;
--- src/router/conditional/index.ts
+++ src/router/conditional/index.ts
@@ -1,3 +1,4 @@
 export { default as CardRouter } from './CardRouter';
+export { default as CardRouterID } from './CardRouterID';
 export { default as LoggedRouter } from './LoggedRouter';
 export { default as PrivateRouter } from './PrivateRouter';
