dadd5e95acbb62aa827ebb7829aac5638210ce6b
luis <47650944+Luisill0@users.noreply.github.com>
2023-08-03 16:30:15 -0600

    new inputs for card form

--- src/components/cardForm/index.ts
+++ src/components/cardForm/index.ts
@@ -3,4 +3,3 @@ export { default as ImageInput } from './ImageInput';
 export { default as DescriptionFormGroup } from './DescriptionFormGroup';
 export { default as RowTextFormGroup } from './RowTextFormGroup';
 export { default as SocialsInput } from './SocialsInput';
-export { default as SocialsTextInputs } from './SocialsTextInputs';
