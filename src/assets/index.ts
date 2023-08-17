fd4152c396411d0de2dc5d4dfb40808f319ac0dc
luis <47650944+Luisill0@users.noreply.github.com>
2023-08-17 14:43:26 -0600

    organize assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,5 @@
-export { default as ACSELogo } from './ACSELogo.webp';
-export { default as ACSELogoDark } from './ACSELogo-dark.webp';
-export { default as CaorIncLogo } from './caor.webp';
-export { default as Meeting } from './meeting.webp';
-export { default as TeamProcess } from './team-process.webp';
-export { default as TeamProcessSM } from './team-process-sm.webp';
-export { default as TrenningLogo } from './trenning.webp';
-export { default as ZoomerChatLogo } from './zoomer.webp';
+export * from './companies';
+export * from './logos';
 export * from './plans';
+export * from './projects';
+export * from './stock';
